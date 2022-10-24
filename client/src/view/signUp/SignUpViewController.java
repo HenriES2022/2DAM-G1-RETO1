55984cb882963b887c4bb725eb4a78a01851bf2f
ioritz <ioritzurkiri2002@gmail.com>
2022-10-24 10:04:23 +0200

    Logger created, validations started, events added

--- client/src/view/signUp/SignUpViewController.java
+++ client/src/view/signUp/SignUpViewController.java
@@ -5,19 +5,20 @@
  */
 package view.signUp;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import com.sun.istack.internal.logging.Logger;
+import java.util.regex.Pattern;
+import javafx.application.Platform;
 import javafx.beans.Observable;
-import javafx.event.EventType;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
  * FXML Controller class
@@ -26,8 +27,7 @@ import javafx.stage.Stage;
  */
 public class SignUpViewController {
 
-   // private static final Logger logger = Logger.getLogger(type);
-    
+    // private static final Logger logger = Logger.getLogger(type);
     @FXML
     private TextField txtFullName;
     @FXML
@@ -40,29 +40,115 @@ public class SignUpViewController {
     private Button btnSignUp;
     @FXML
     private Button btnBack;
-    
+
     private Stage myStage = null;
     private Scene myScene = null;
-    
-    public void initStage(Parent root){
+    private Boolean correctEmail = false;
+    private Boolean correctPassword = false;
+    private static Alert alert = null;
+    private static final Logger LOG = Logger.getLogger(view.signUp.SignUpViewController.class);;
+
+    /**
+     * This method startes the Sign Up window
+     * @param root The scene that is going to be loaded in the stage
+     */
+    public void initStage(Parent root) {
         myScene = new Scene(root);
         myStage = new Stage();
-        
+
         myStage.setTitle("Registro");
         myStage.setScene(myScene);
         myStage.setResizable(false);
-        myStage.showAndWait();
-        
+
         btnSignUp.setDisable(true);
         btnBack.setDisable(false);
-        
+
+        txtFullName.requestFocus();
+
         txtFullName.focusedProperty().addListener((Observable focusChanged) -> {
-            validateText();
+            validateText(txtFullName);
         });
+
+        txtUsername.focusedProperty().addListener((Observable focusChanged) -> {
+            validateText(txtUsername);
+        });
+
+        txtEmail.focusedProperty().addListener((Observable focusChanged) -> {
+            validateText(txtEmail);
+        });
+
+        txtPassword.focusedProperty().addListener((Observable focusChanged) -> {
+            validateText(txtPassword);
+        });
+        
+        myStage.setOnCloseRequest((WindowEvent windowEvent) -> {
+            LOG.info("Opening exit alert confirmation");
+            alert = new Alert(Alert.AlertType.CONFIRMATION, "Quieres cerrar el programa?", ButtonType.YES, ButtonType.NO);
+            alert.showAndWait();
+            
+            if (alert.getResult() == ButtonType.YES) {
+                Platform.exit();
+            } else{
+                windowEvent.consume();
+            }
+        });
+        myStage.showAndWait();
     }
 
-    private void validateText() {
+    /**
+     * This methods validates that the text in the text fields are correct
+     * @param field The text field that we are going to check
+     */
+    private void validateText(TextField field) {
         
+        if (txtEmail.getText().trim().equalsIgnoreCase("")
+                || txtPassword.getText().trim().equalsIgnoreCase("")
+                || txtFullName.getText().trim().equalsIgnoreCase("")
+                || txtUsername.getText().trim().equalsIgnoreCase("")) {
+
+            btnSignUp.setDisable(true);
+        } else {
+            btnSignUp.setDisable(false);
+            if (field.equals(txtFullName) && field.getText().length()>100) {
+                btnSignUp.setDisable(true);
+                alert = new Alert(Alert.AlertType.ERROR, "El nombre completo no puede tener mas de 100 caracteres");
+            }
+            
+            if (field.equals(txtPassword)) {
+                correctPassword = passwordValidator(txtPassword.getText());
+            }
+            if (field.equals(txtEmail)) {
+                correctEmail = emailValidator(txtEmail.getText());
+                if (correctEmail) {
+                    
+                } 
+            }
+        }
+
     }
-    
+
+    /**
+     * This method validates that the introduced email is sintacticaly correct
+     * @param email The email we are going to check
+     * @return Returns True if the email matches the pattern we use to check the email, False if dosen't match
+     */
+    private Boolean emailValidator(String email) {
+        if (email.length() <= 100) {
+            return Pattern.compile("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+                + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$")
+                .matcher(email)
+                .matches();
+        }
+        return false;
+    }
+
+    /**
+     * this method validates that the password is correct
+     * @param password The password we are going to validate
+     * @return Returns True if the password is correct, False if is not.
+     */
+    private Boolean passwordValidator(String password) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
