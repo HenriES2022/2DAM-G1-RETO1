484ebf7d6015a25104f74652fe6a637078a4408f
ioritz <ioritzurkiri2002@gmail.com>
2022-10-25 10:08:02 +0200

    Sign Up view controller modified, added signUp method and more controls, loggers, alerts

--- client/src/clientProject/view/signUp/SignUpViewController.java
+++ client/src/clientProject/view/signUp/SignUpViewController.java
@@ -6,6 +6,8 @@
 package clientProject.view.signUp;
 
 import com.sun.istack.internal.logging.Logger;
+import enumerations.Operation;
+import exceptions.ServerErrorException;
 import java.util.regex.Pattern;
 import javafx.application.Platform;
 import javafx.beans.Observable;
@@ -17,8 +19,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import model.Message;
+import model.User;
 
 /**
  * FXML Controller class
@@ -45,22 +50,27 @@ public class SignUpViewController {
     private Scene myScene = null;
     private Boolean correctEmail = false;
     private Boolean correctPassword = false;
+    private Boolean correctFullName = true;
+    private Boolean correctUserName = true;
+    private ClientSocketFactory myFactory;
+    private ClientSocket clientSocket;
     private static Alert alert = null;
     private static final Logger LOG = Logger.getLogger(clientProject.view.signUp.SignUpViewController.class);
 
-    ;
-
     /**
      * This method startes the Sign Up window
+     *
      * @param root The scene that is going to be loaded in the stage
      */
     public void initStage(Parent root) {
+        LOG.info("Starting the window and setting the components on the screen");
         myScene = new Scene(root);
         myStage = new Stage();
 
         myStage.setTitle("Registro");
         myStage.setScene(myScene);
         myStage.setResizable(false);
+        myStage.initModality(Modality.WINDOW_MODAL);
 
         btnSignUp.setDisable(true);
         btnBack.setDisable(false);
@@ -83,18 +93,28 @@ public class SignUpViewController {
             validateText(txtPassword);
         });
 
+        btnBack.setOnAction((ActionEvent) -> {
+            LOG.info("Closing the window");
+            myStage.close();
+        });
+
+        btnSignUp.setOnAction((ActionEvent) -> {
+            signUp();
+        });
+
         myStage.setOnCloseRequest((WindowEvent windowEvent) -> {
             LOG.info("Opening exit alert confirmation");
             alert = new Alert(Alert.AlertType.CONFIRMATION, "Quieres cerrar el programa?", ButtonType.YES, ButtonType.NO);
             alert.showAndWait();
 
             if (alert.getResult() == ButtonType.YES) {
+                LOG.info("Closing the application");
                 Platform.exit();
             } else {
+                LOG.info("Canceled application close");
                 windowEvent.consume();
             }
         });
-        myStage.showAndWait();
     }
 
     /**
@@ -103,7 +123,7 @@ public class SignUpViewController {
      * @param field The text field that we are going to check
      */
     private void validateText(TextField field) {
-
+        LOG.info("Validating the fields isn't empty");
         if (txtEmail.getText().trim().equalsIgnoreCase("")
                 || txtPassword.getText().trim().equalsIgnoreCase("")
                 || txtFullName.getText().trim().equalsIgnoreCase("")
@@ -112,22 +132,50 @@ public class SignUpViewController {
             btnSignUp.setDisable(true);
         }
 
-        if (field.equals(txtFullName) && field.getText().length() > 100) {
-            btnSignUp.setDisable(true);
-            alert = new Alert(Alert.AlertType.ERROR, "El nombre completo no puede tener mas de 100 caracteres");
+        if (field.equals(txtFullName)) {
+            LOG.info("Validating the full name field");
+            if (field.getText().length() > 100) {
+                btnSignUp.setDisable(true);
+                alert = new Alert(Alert.AlertType.ERROR, "El nombre completo no puede tener mas de 100 caracteres");
+                correctFullName = false;
+            } else {
+                correctFullName = true;
+                btnSignUp.setDisable(true);
+                alert = new Alert(Alert.AlertType.ERROR, "El nombre completo no puede tener mas de 100 caracteres");
+            }
+
         }
 
         if (field.equals(txtPassword)) {
+            LOG.info("Validating the password field");
             correctPassword = passwordValidator(txtPassword.getText());
-            btnSignUp.setDisable(true);
+            if (!correctPassword) {
+                btnSignUp.setDisable(true);
+            }
         }
         if (field.equals(txtEmail)) {
+            LOG.info("Validationg the email field");
             correctEmail = emailValidator(txtEmail.getText());
-            if (correctEmail) {
-
+            if (!correctEmail) {
+                alert = new Alert(Alert.AlertType.ERROR, "El email no es correcto", ButtonType.OK);
             }
         }
 
+        if (field.equals(txtUsername)) {
+            LOG.info("Validating the username field");
+            if (field.getText().length() > 50) {
+                btnSignUp.setDisable(true);
+                alert = new Alert(Alert.AlertType.ERROR, "El nombre de usuario debe de tener menos de 50 caracteres", ButtonType.OK);
+                correctUserName = false;
+            } else {
+                correctUserName = true;
+            }
+
+        }
+
+        if (correctEmail && correctFullName && correctPassword && correctUserName) {
+            btnSignUp.setDisable(false);
+        }
     }
 
     /**
@@ -187,4 +235,46 @@ public class SignUpViewController {
         return false;
     }
 
+    /**
+     * This method sends de user to the server to register it
+     */
+    private void signUp() {
+        LOG.info("Starting the sign up and setting up all equired objects");
+        Message message = null;
+        User user = null;
+        Operation operation = null;
+
+        LOG.info("Setting up the required variables");
+        clientSocket = myFactory.getImplementation();
+        user = new User();
+        user.setFullName(txtFullName.getText());
+        user.setEmail(txtEmail.getText());
+        user.setPassword(txtPassword.getText());
+        user.setLogin(txtUsername.getText());
+
+        operation = Operation.SING_UP;
+
+        message = new Message();
+        message.setUserData(user);
+        message.setOperation(operation);
+
+        try {
+            message = clientSocket.connectToServer(message);
+
+            operation = message.getOperation();
+
+            if (operation.equals(Operation.USER_EXISTS)) {
+                alert = new Alert(Alert.AlertType.ERROR, "El usuario ya existe, pruebe con otro", ButtonType.OK);
+                LOG.warning("The user attemped to sign up all ready exists");
+            } else if (operation.equals(Operation.OK)) {
+                alert = new Alert(Alert.AlertType.INFORMATION, "El usuario ha sido registrado correctamente", ButtonType.OK);
+                LOG.info("The sign up has be done correctly. Exiting method...");
+            }
+        } catch (ServerErrorException e) {
+            LOG.severe(e.getMessage());
+            alert = new Alert(Alert.AlertType.ERROR, "Error al conectarse con el servidor, intentelo de nuevo mas tarde", ButtonType.OK);
+
+        }
+
+    }
 }
