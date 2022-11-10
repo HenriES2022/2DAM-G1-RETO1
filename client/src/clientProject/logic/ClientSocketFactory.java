48c969430545f2de04388202bf5e76eef074c5d7
ioritz <ioritzurkiri2002@gmail.com>
2022-11-10 10:26:55 +0100

    Javadoc modificado y actualizado en el cliente

--- client/src/clientProject/logic/ClientSocketFactory.java
+++ client/src/clientProject/logic/ClientSocketFactory.java
@@ -8,14 +8,23 @@ package clientProject.logic;
 import java.util.ResourceBundle;
 
 /**
- *
+ *This is the factory that creates  ClientSocket implementations 
  * @author Joritz
  */
 public class ClientSocketFactory {
-
+    /**
+     * The string with the connection type that the client side will use when is going to connect to the server. Obtained from a properties file.
+     */
     private static final String CONNECTION_TYPE = ResourceBundle.getBundle("clientProject.configClient").getString("connection_type");
+    /**
+     * To compare if obtained connection type from the properties is the same to this.
+     */
     private static final String CONNECTION = "socket";
-
+    /**
+     * Obtains the implementation of Client Socket interface
+     * @return Returns a new ClientSocketImplementation if CONNECTION_TYPE is the same to the connection type we want, <br>
+     * or null if the readed CONNECTION_TYPE don't exists.
+     */
     public static ClientSocket getImplementation() {
         if (CONNECTION_TYPE.equalsIgnoreCase(CONNECTION)) {
             return new ClientSocketImplementation();
--- client/src/clientProject/logic/ClientSocketImplementation.java
+++ client/src/clientProject/logic/ClientSocketImplementation.java
@@ -23,8 +23,17 @@ import model.Message;
  */
 public class ClientSocketImplementation implements ClientSocket {
     
+    /**
+     * This is the logger of the class.
+     */
     private static final Logger LOG = Logger.getLogger("clientProject.logic.ClientSocketImplementation");
+    /**
+     * The ip addres of the server, readed from a properties file.
+     */
     private static final String HOST = ResourceBundle.getBundle("clientProject.configClient").getString("ip_address");
+    /**
+     * The port where the server is listening, readed from a properties file.
+     */
     private static final Integer PORT = Integer.parseInt(ResourceBundle.getBundle("clientProject.configClient").getString("port"));
     
     @Override
@@ -32,27 +41,31 @@ public class ClientSocketImplementation implements ClientSocket {
         Socket socket = null;
         try {
             LOG.info("Establishing the connection to the server");
-            // Se crea la conexión al servidor
+            // Create a connection to the server
             socket = new Socket(HOST, PORT);
             
             LOG.info("Connected: Opening I/O streams");
-            // Se abren los streams de E/S
+            // Openning the I/O streams
             ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
             ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
             
             LOG.info("Sending message to the server");
-            // Envia el mensaje al servidor
+            // Sending the message to the server
             oos.writeObject(message);
             
             LOG.info("Reading message from the server");
-            // Recibe la respuesta del servidor
+            // Receiving respones from the server
             Message respuesta = (Message) ois.readObject();
             
+            
             switch (respuesta.getOperation()) {
+                //If the received message says that the server is full
                 case SERVER_FULL:
                     throw new ServerFullException("El servdor esta lleno, intentelo mas tarde");
+                //If the received message says that any error has happend in the server
                 case SERVER_ERROR:
                     throw new ServerErrorException("Error al conectarse con el servidor, intentelo de nuevo mas tarde");
+                //Any other case
                 default:
                     return respuesta;
             }
