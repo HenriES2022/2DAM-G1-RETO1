279ca124c63acf39cc8446f392fec8a0483883ff
Henr1ES <48323104+HenriES2022@users.noreply.github.com>
2022-11-11 09:35:55 +0100

    Add files via upload

---
+++ 2dam_reto1_g1.sql
@@ -0,0 +1,89 @@
+﻿-- MySQL dump 10.13  Distrib 5.7.29, for Win64 (x86_64)
+--
+-- Host: localhost    Database: 2dam_reto1_g1
+-- ------------------------------------------------------
+-- Server version	5.7.29-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+DROP SCHEMA IF EXISTS 2dam_reto1_g1;
+CREATE DATABASE 2dam_reto1_g1;
+USE 2dam_reto1_g1;
+
+
+--
+-- Table structure for table `signin`
+--
+
+DROP TABLE IF EXISTS `signin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `signin` (
+  `LAST_SIGIN` timestamp NOT NULL,
+  `USER_ID` int(5) NOT NULL,
+  PRIMARY KEY (`LAST_SIGIN`),
+  KEY `USER_ID` (`USER_ID`),
+  CONSTRAINT `signin_ibfk_1` FOREIGN KEY (`USER_ID`) REFERENCES `user` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `signin`
+--
+
+LOCK TABLES `signin` WRITE;
+/*!40000 ALTER TABLE `signin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `signin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `ID` int(5) NOT NULL AUTO_INCREMENT,
+  `LOGIN` varchar(50) NOT NULL,
+  `EMAIL` varchar(100) NOT NULL,
+  `FULL_NAME` varchar(100) NOT NULL,
+  `CURRENT_STATUS` ENUM('ENABLED', 'DISABLED') NOT NULL DEFAULT 'ENABLED',
+  `PRIVILEGE` ENUM('USER', 'ADMIN') NOT NULL DEFAULT 'USER',
+  `USER_PASSWORD` varchar(100) NOT NULL,
+  `LAST_PASSWORD_CHANGE` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+INSERT INTO user(LOGIN, EMAIL, FULL_NAME, USER_PASSWORD)
+values('Henrique', 'henriqueyeguo@gmail.com', 'Henrique Yeguo', MD5('Password1234?')),('Joritz', 'joritzjimeno@gmail.com', 'Joritz Jimeno', MD5('Password1234?'));
+
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-19 13:22:50
